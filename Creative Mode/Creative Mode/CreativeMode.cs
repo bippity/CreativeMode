d634477d70ebd8f1c5dd05f96662aaead32aad5e
Bippity <bippity@users.noreply.github.com>
2014-05-09 21:14:52 -0500

    Version tick to 1.16

--- Creative Mode/Creative Mode/CreativeMode.cs
+++ Creative Mode/Creative Mode/CreativeMode.cs
@@ -13,7 +13,7 @@ using TShockAPI.DB;
 
 namespace CreativeMode
 {
-    [ApiVersion(1, 15)]
+    [ApiVersion(1, 16)]
     public class CreativeMode : TerrariaPlugin
     {
         public static Boolean[] playerList = new Boolean[Main.maxNetPlayers];
