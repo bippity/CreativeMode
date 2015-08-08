d675195fa338b54caa93206eb95e1b05753e7f4c
Bippity <alex.waang@yahoo.com>
2015-08-08 00:54:48 -0500

    Api tick 1.21
    Tickity Tock

--- Creative Mode/Creative Mode/CreativeMode.cs
+++ Creative Mode/Creative Mode/CreativeMode.cs
@@ -13,7 +13,7 @@ using TShockAPI.DB;
 
 namespace CreativeMode
 {
-    [ApiVersion(1, 20)]
+    [ApiVersion(1, 21)]
     public class CreativeMode : TerrariaPlugin
     {
         public static Boolean[] playerList = new Boolean[Main.maxNetPlayers];
